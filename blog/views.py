e8cd4aefe82555b5764efe6eddc98f9574c4eb00
tito-k <komolafetoyin17@gmail.com>
2020-04-26 01:39:52 +0100

    'final'

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
 
-
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -26,7 +25,7 @@ class BlogUpdateView(UpdateView):
     fields = ['title', 'body']
 
 
-class BlogDeleteView(DeleteView):  # new
+class BlogDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
